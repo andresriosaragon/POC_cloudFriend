3acb361aee6acd402251401093fd6a4a8c742991
splinermann <andresriosragon@gmail.com>
2021-08-29 16:09:41 +1200

    test1

--- cloudfriend/build.ts
+++ cloudfriend/build.ts
@@ -6,11 +6,39 @@ import { stringify } from "yaml";
 // const merged = cf.merge(queue); dsads
 const lambda = makeLambda("myFunction", "build/myFunction");
 const httpApi = makeHTTPApi();
-const data = stringify(httpApi);
-const data2 = stringify(lambda);
+const api = stringify(httpApi);
+/*
+AWSTemplateFormatVersion: "2010-09-09"
+Description: Resources for user entitlement service
+
+Parameters:
+  SystemName:
+    Description: System Name
+    Type: String
+  LayerName:
+    Description: Name of the Layer
+    Type: String
+  GroupName:
+    Description: Service name
+    Type: String
+*/
+const data = {
+  AWSTemplateFormatVersion: "2010-09-09",
+  Description: "Resources for user entitlement service",
+  Parameters: {
+    SystemName: {
+      Type: "String",
+      Default: "system",
+    },
+  },
+  Resources: {
+    MyApi: httpApi,
+  },
+};
+const data2 = stringify(data);
 console.log(JSON.stringify(httpApi, null, 2));
 
-writeFileSync("cloudformation.yml", data);
-writeFileSync("cloudformation2.yml", data2);
+writeFileSync("cloudformation.yml", data2);
+// writeFileSync("cloudformation2.yml", data2);
 
 // console.log(queue);
