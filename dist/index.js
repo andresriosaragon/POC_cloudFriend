a4c39ad6c57607b43cf98a2560e70ed15fdb6f67
splinermann <andresriosragon@gmail.com>
2021-08-29 19:11:40 +1200

    test1

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 const handler = async (event) => {
-  console.log("crasaeste = ", JSON.stringify(event));
+  console.log("crasaesste = ", JSON.stringify(event));
 };
 
 module.exports = { handler };
