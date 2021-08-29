a4ee119b6268506fb2f0292f98a04754a2c8fd1d
splinermann <andresriosragon@gmail.com>
2021-08-29 17:58:10 +1200

    test1

--- packages/createEntitlement.js
+++
@@ -1,5 +0,0 @@
-const handler = async (event) => {
-  console.log("create = ", JSON.stringify(event));
-};
-
-module.exports = { handler };
--- packages/getEntitlement.ts
+++
@@ -1,5 +0,0 @@
-const handler = async (event) => {
-  console.log("event = ", JSON.stringify(event));
-};
-
-export { handler };
